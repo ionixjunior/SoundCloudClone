60e5b4e9b1f91b400ac3b424d1b81f6f55f0bfcd
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-22 10:22:27 -0300

    Criar projeto

---
+++ SoundCloudClone/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SoundCloudClone.Services;
+using SoundCloudClone.Views;
+
+namespace SoundCloudClone
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SoundCloudClone/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SoundCloudClone.Models;
+
+namespace SoundCloudClone.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
