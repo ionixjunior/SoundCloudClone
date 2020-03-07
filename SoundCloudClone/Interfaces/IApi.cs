f4f0c8c1cac619ec12855645dcf96115975a6989
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-07 10:43:32 -0300

    Implementar serviço para carregar os álbuns

--- SoundCloudClone/Interfaces/IApi.cs
+++ SoundCloudClone/Interfaces/IApi.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Refit;
 using SoundCloudClone.Models;
 
 namespace SoundCloudClone.Interfaces
 {
     public interface IApi
     {
+        [Get("/home")]
         Task<Home> GetAlbums();
     }
 }
---
+++ SoundCloudClone/Services/ApiService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+using SoundCloudClone.Interfaces;
+using SoundCloudClone.Models;
+
+namespace SoundCloudClone.Services
+{
+    public class ApiService : IApi
+    {
+        private readonly IApi _api;
+
+        public ApiService()
+        {
+            _api = RestService.For<IApi>("https://soundcloudclone.getsandbox.com");
+        }
+
+        public async Task<Home> GetAlbums()
+        {
+            return await _api.GetAlbums();
+        }
+    }
+}
