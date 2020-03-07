7919e2e1439f9fbea618dbc964eebe8dd9ee5901
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-07 10:38:39 -0300

    Mapear serviço para carregar os álbuns

---
+++ SoundCloudClone/Interfaces/IApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using SoundCloudClone.Models;
+
+namespace SoundCloudClone.Interfaces
+{
+    public interface IApi
+    {
+        Task<Home> GetAlbums();
+    }
+}
---
+++ SoundCloudClone/Models/Home.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SoundCloudClone.Models
+{
+    public class Album
+    {
+        [JsonProperty("artwork_url_template")]
+        public string ArtworkUrlTemplate { get; set; }
+
+        [JsonProperty("artwork_style")]
+        public string ArtworkStyle { get; set; }
+
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("short_title")]
+        public string ShortTitle { get; set; }
+
+        [JsonProperty("short_subtitle")]
+        public string ShortSubtitle { get; set; }
+    }
+
+    public class AlbumCollection
+    {
+        [JsonProperty("collection")]
+        public IList<Album> Albums { get; set; }
+    }
+
+    public class AlgumGroup
+    {
+        [JsonProperty("style")]
+        public string Style { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("selection_items")]
+        public AlbumCollection AlbumCollection { get; set; }
+    }
+
+    public class AlbumGroupCollection
+    {
+        [JsonProperty("multiple_content_selection_card")]
+        public AlgumGroup AlbumGroup { get; set; }
+    }
+
+    public class Home
+    {
+        [JsonProperty("collection")]
+        public IList<AlbumGroupCollection> AlgumGroupsCollection { get; set; }
+    }
+}
