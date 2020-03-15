2e6062a9641575150aa6b8f478f447532b36ddae
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-15 10:59:09 -0300

    Mapear classe de stream do domínio do app

---
+++ SoundCloudClone/Models/App/Stream.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SoundCloudClone.Models.App
+{
+    public class Stream
+    {
+        public long FullDuration { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Title { get; set; }
+        public int PlaybackCount { get; set; }
+        public int CommentsCount { get; set; }
+        public int RepostsCount { get; set; }
+        public int LikesCount { get; set; }
+        public string Username { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Genre { get; set; }
+        public string ArtworkUrlTemplate { get; set; }
+    }
+}
