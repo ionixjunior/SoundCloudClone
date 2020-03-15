0d1d3133d66c77f73778b714362b3205b05e48ec
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-15 11:23:57 -0300

    Implementar carregamento dos streams

--- SoundCloudClone/ViewModels/StreamViewModel.cs
+++ SoundCloudClone/ViewModels/StreamViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Threading.Tasks;
 using SoundCloudClone.Interfaces;
+using SoundCloudClone.Extensions;
+using MvvmHelpers;
+using SoundCloudClone.Models.App;
 
 namespace SoundCloudClone.ViewModels
 {
-    public class StreamViewModel : IInitialize
+    public class StreamViewModel : BaseViewModel, IInitialize
     {
         private readonly IApi _api;
         private bool _alreadyInitialized = false;
 
+        public ObservableRangeCollection<Stream> Streams { get; private set; }
+
         public StreamViewModel(IApi api)
         {
             _api = api;
+
+            Streams = new ObservableRangeCollection<Stream>();
         }
 
         public async Task InitializeAsync()
@@ -19,7 +26,17 @@ namespace SoundCloudClone.ViewModels
             if (_alreadyInitialized)
                 return;
 
-            // TODO CARREGAR AS INFORMAÇÕES
+            try
+            {
+                var streamApi = await _api.GetStreams();
+                var streamApp = streamApi.ToStreamApp();
+
+                Streams.AddRange(streamApp);
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }
 
             _alreadyInitialized = true;
         }
