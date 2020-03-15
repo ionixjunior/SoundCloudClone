87fd7b98017661a4b989a8592dcea7b182b74464
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-15 11:16:13 -0300

    Implementar método de extensão para transformar stream API em app

---
+++ SoundCloudClone/Extensions/StreamExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SoundCloudClone.Models.App;
+
+namespace SoundCloudClone.Extensions
+{
+    public static class StreamExtension
+    {
+        public static IList<Stream> ToStreamApp(this SoundCloudClone.Models.Api.Stream streamApi)
+        {
+            var streams = new List<Stream>();
+
+            foreach (var collection in streamApi.Collection)
+                streams.Add(new Stream(collection));
+
+            return streams;
+        }
+    }
+}
