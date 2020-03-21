c984688f35f0665fdc150037af09d19f67e97e74
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-21 13:00:58 -0300

    Criar conversor para exibir countador das mídias sociais

---
+++ SoundCloudClone/Converters/SocialCounterToTextConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Converters
+{
+    public class SocialCounterToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int counter)
+            {
+                if (counter is 0)
+                    return string.Empty;
+
+                return $"  {counter}";
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
