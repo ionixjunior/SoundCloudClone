b3691d709cdd7715c0eae15af7087ef80487a787
Ione Souza Junior <junior@ionixjunior.com.br>
2020-03-21 12:43:15 -0300

    Implementar conversor para exibir data de publicação do streaming

---
+++ SoundCloudClone/Converters/DateTimeToTextConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Humanizer;
+
+namespace SoundCloudClone.Converters
+{
+    public class DateTimeToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+                return dateTime.Humanize();
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
