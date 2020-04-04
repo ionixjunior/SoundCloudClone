6e6f52a5f90653a62429ed567a5f6a71c70143f8
Ione Souza Junior <junior@ionixjunior.com.br>
2020-04-04 10:24:17 -0300

    Criar custom render para SearchView no iOS

---
+++ SoundCloudClone.iOS/Renderers/SearchViewRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SoundCloudClone.iOS.Renderers;
+using SoundCloudClone.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SearchView), typeof(SearchViewRenderer))]
+namespace SoundCloudClone.iOS.Renderers
+{
+    public class SearchViewRenderer : PageRenderer
+    {
+    }
+}
