10a7f17b9de1c6989c9039459a0ae05a7cd7e5ae
Ione Souza Junior <junior@ionixjunior.com.br>
2020-04-04 11:51:00 -0300

    Implementar consumo do evento usando extensão reativa

--- SoundCloudClone/ViewModels/SearchViewModel.cs
+++ SoundCloudClone/ViewModels/SearchViewModel.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.Reactive.Linq;
+
 namespace SoundCloudClone.ViewModels
 {
     public class SearchViewModel
     {
+        public event EventHandler<string> SearchTextChanged;
+
         public SearchViewModel()
         {
+            Observable
+                .FromEventPattern<string>(
+                    x => SearchTextChanged += x,
+                    x => SearchTextChanged -= x)
+                .Throttle(TimeSpan.FromSeconds(1))
+                .Select(x => x.EventArgs)
+                .Subscribe(OnSearchTextChanged);
         }
 
-        public void SearchBy(string text)
+        private void OnSearchTextChanged(string text)
         {
-            // TODO IMPLEMENTAR BUSCA
-            System.Diagnostics.Debug.WriteLine($"Vai procurar por {text} na viewmodel");
+            // TODO IMPLEMENTAR A CONSULTA NA API
+            System.Diagnostics.Debug.WriteLine($"Buscando view evento o texto {text}");
         }
+
+        public void SearchBy(string text) => SearchTextChanged.Invoke(this, text);
     }
 }
