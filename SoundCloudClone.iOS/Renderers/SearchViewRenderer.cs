399565e5b1551e400bd3386dbb26700f1cfde3ff
Ione Souza Junior <junior@ionixjunior.com.br>
2020-04-04 10:35:47 -0300

    Adicionar UISearchController na NavigationBar do iOS

--- SoundCloudClone.iOS/Renderers/SearchViewRenderer.cs
+++ SoundCloudClone.iOS/Renderers/SearchViewRenderer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using SoundCloudClone.iOS.Renderers;
 using SoundCloudClone.Views;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -9,5 +10,20 @@ namespace SoundCloudClone.iOS.Renderers
 {
     public class SearchViewRenderer : PageRenderer
     {
+        private UISearchController _searchController;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _searchController = new UISearchController();
+        }
+
+        public override void WillMoveToParentViewController(UIViewController parent)
+        {
+            base.WillMoveToParentViewController(parent);
+
+            parent.NavigationItem.SearchController = _searchController;
+        }
     }
 }
