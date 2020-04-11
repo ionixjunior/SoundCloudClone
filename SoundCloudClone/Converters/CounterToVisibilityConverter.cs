75c26fb656cd795380227ead29d089fd9181c411
Ione Souza Junior <junior@ionixjunior.com.br>
2020-04-11 13:16:18 -0300

    Criar conversor de visibilidade

---
+++ SoundCloudClone/Converters/CounterToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Converters
+{
+    public class CounterToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int counter)
+                return counter == 0 ? false : true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
