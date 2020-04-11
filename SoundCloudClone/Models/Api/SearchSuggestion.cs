c0d767ba1a0c65496af1c6e65506dd98f749cdc8
Ione Souza Junior <junior@ionixjunior.com.br>
2020-04-11 11:10:14 -0300

    Mapear model para deserializar payload das sugestões da busca

---
+++ SoundCloudClone/Models/Api/SearchSuggestion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SoundCloudClone.Models.Api
+{
+    public class SearchSuggestionCollection
+    {
+        [JsonProperty("output")]
+        public string Output { get; set; }
+    }
+
+    public class SearchSuggestion
+    {
+        [JsonProperty("collection")]
+        public IList<SearchSuggestionCollection> Collection { get; set; }
+    }
+}
