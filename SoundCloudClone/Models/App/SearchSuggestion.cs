9573cee3670d74c626741f12bf5b0a9f49b01a1b
Ione Souza Junior <junior@ionixjunior.com.br>
2020-04-11 11:28:53 -0300

    Mapear classe para representar sugestões de busca no app

---
+++ SoundCloudClone/Models/App/SearchSuggestion.cs
@@ -0,0 +1,14 @@
+﻿using SoundCloudClone.Models.Api;
+
+namespace SoundCloudClone.Models.App
+{
+    public class SearchSuggestion
+    {
+        public string Description { get; private set; }
+
+        public SearchSuggestion(SearchSuggestionCollection searchSuggestionApi)
+        {
+            Description = searchSuggestionApi.Output;
+        }
+    }
+}
