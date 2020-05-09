dfba3fdf8e6586dd7e60c5d4eefddd56889fa2d8
Ione Souza Junior <junior@ionixjunior.com.br>
2020-05-09 10:29:02 -0300

    Criar estilo light no app

---
+++ SoundCloudClone/Styles/LightStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Styles
+{
+    public partial class LightStyle : ResourceDictionary
+    {
+        public LightStyle()
+        {
+            InitializeComponent();
+        }
+    }
+}
