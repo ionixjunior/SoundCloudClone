10f304438c0fe816d1db4cb84468b51c7f74f8b9
Ione Souza Junior <junior@ionixjunior.com.br>
2020-05-09 10:50:15 -0300

    Criar estilo base para o app

---
+++ SoundCloudClone/Styles/BaseStyle.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Styles
+{
+    public partial class BaseStyle : ResourceDictionary
+    {
+        public BaseStyle()
+        {
+            InitializeComponent();
+        }
+    }
+}
