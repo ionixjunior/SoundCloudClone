e4602fdc5518269c10aa67518149c2d7d530ab72
Ione Souza Junior <junior@ionixjunior.com.br>
2020-05-23 13:06:37 -0300

    Adicionar constante para a fonte de texto padrão no iOS

---
+++ SoundCloudClone.iOS/Constants.cs
@@ -0,0 +1,9 @@
+﻿using UIKit;
+
+namespace SoundCloudClone.iOS
+{
+    public static class Constants
+    {
+        public static UIFont DefaultFont = UIFont.FromName("Interstate Light Cond", 20);
+    }
+}
