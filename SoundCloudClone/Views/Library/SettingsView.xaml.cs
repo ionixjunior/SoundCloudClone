c5344223b92110bf81d6e11bc6f2bfa5edc73cb4
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-13 11:47:05 -0300

    Adicionar tela settings

---
+++ SoundCloudClone/Views/Library/SettingsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Views.Library
+{
+    public partial class SettingsView : ContentPage
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
