34ed2df1a0cc7f357cc7185d6f9999d4185e78f2
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-20 10:21:54 -0300

    Criar serviço para carregar os temas

---
+++ SoundCloudClone/Services/ThemeService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SoundCloudClone.Enums;
+
+namespace SoundCloudClone.Services
+{
+    public class ThemeService
+    {
+        public IList<ThemeEnum> GetOptions()
+        {
+            return new List<ThemeEnum>
+            {
+                ThemeEnum.Light,
+                ThemeEnum.Dark
+            };
+        }
+    }
+}
