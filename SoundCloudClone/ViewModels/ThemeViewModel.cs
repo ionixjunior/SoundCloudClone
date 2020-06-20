e7c65c929c67bcf7d1dd33217b37e0879bd539d1
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-20 11:51:53 -0300

    Implementar comando para selecionar item

--- SoundCloudClone/ViewModels/ThemeViewModel.cs
+++ SoundCloudClone/ViewModels/ThemeViewModel.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using MvvmHelpers;
 using SoundCloudClone.Interfaces;
 using SoundCloudClone.Models.App;
+using Xamarin.Forms;
 
 namespace SoundCloudClone.ViewModels
 {
     public class ThemeViewModel : BaseViewModel
     {
         public IList<Theme> Options { get; private set; }
+        public Command OptionSelectedCommand { get; private set; }
 
         public ThemeViewModel(ITheme theme)
         {
             Options = theme.GetOptions();
+            OptionSelectedCommand = new Command(OptionSelected);
+        }
+
+        private void OptionSelected()
+        {
+            System.Diagnostics.Debug.WriteLine("INVOCOU O COMANDO");
         }
     }
 }
