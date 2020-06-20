d3b583870f24b3457391c60bdfdcab68c16389a1
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-20 11:04:17 -0300

    Criar propriedade Options para fazer binding para a tela

--- SoundCloudClone/ViewModels/ThemeViewModel.cs
+++ SoundCloudClone/ViewModels/ThemeViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using MvvmHelpers;
+using SoundCloudClone.Enums;
 using SoundCloudClone.Interfaces;
 
 namespace SoundCloudClone.ViewModels
 {
     public class ThemeViewModel : BaseViewModel
     {
+        public IList<ThemeEnum> Options { get; private set; }
+
         public ThemeViewModel(ITheme theme)
         {
-            var options = theme.GetOptions();
+            Options = theme.GetOptions();
         }
     }
 }
