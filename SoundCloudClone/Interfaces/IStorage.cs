3a9b969d6027d3281d2031b1b46f98db59c9c670
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-27 11:41:18 -0300

    Implementar storage service

---
+++ SoundCloudClone/Interfaces/IStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace SoundCloudClone.Interfaces
+{
+    public interface IStorage
+    {
+        int Get(string name, int defaultValue);
+        void Set(string name, int value);
+    }
+}
---
+++ SoundCloudClone/Services/StorageService.cs
@@ -0,0 +1,18 @@
+﻿using SoundCloudClone.Interfaces;
+using Xamarin.Essentials;
+
+namespace SoundCloudClone.Services
+{
+    public class StorageService : IStorage
+    {
+        public int Get(string name, int defaultValue)
+        {
+            return Preferences.Get(name, defaultValue);
+        }
+
+        public void Set(string name, int value)
+        {
+            Preferences.Set(name, value);
+        }
+    }
+}
