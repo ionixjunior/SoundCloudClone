84f623684f14ff664a6c1a0a06fe777674f4f4e3
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-27 13:03:37 -0300

    Refatorar seleção do tema do sistema

--- SoundCloudClone/Services/ThemeService.cs
+++ SoundCloudClone/Services/ThemeService.cs
@@ -31,25 +31,35 @@ namespace SoundCloudClone.Services
             switch (theme)
             {
                 case ThemeEnum.Light:
-                    Application.Current.Resources = new LightStyle();
+                    ChangeByAppStyle(new LightStyle());
                     break;
                 case ThemeEnum.Dark:
-                    Application.Current.Resources = new DarkStyle();
+                    ChangeByAppStyle(new DarkStyle());
                     break;
                 case ThemeEnum.System:
-                    var systemTheme = AppInfo.RequestedTheme;
-
-                    if (systemTheme == AppTheme.Light)
-                        Application.Current.Resources = new LightStyle();
-
-                    if (systemTheme == AppTheme.Dark)
-                        Application.Current.Resources = new DarkStyle();
-
-                    if (systemTheme == AppTheme.Unspecified)
-                        Application.Current.Resources = new LightStyle();
-
+                    ChangeBySystem();
                     break;
             }
         }
+
+        private void ChangeByAppStyle(ResourceDictionary style)
+        {
+            Application.Current.Resources = style;
+        }
+
+        private void ChangeBySystem()
+        {
+            var systemTheme = AppInfo.RequestedTheme;
+
+            if (systemTheme == AppTheme.Light)
+                ChangeByAppStyle(new LightStyle());
+
+            if (systemTheme == AppTheme.Dark)
+                ChangeByAppStyle(new DarkStyle());
+
+            if (systemTheme == AppTheme.Unspecified)
+                ChangeByAppStyle(new LightStyle());
+        }
+
     }
 }
