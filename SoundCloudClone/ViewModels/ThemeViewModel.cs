7bb75ddf8dc1ad8da9a37773b4f61f94fa4fd696
Ione Souza Junior <junior@ionixjunior.com.br>
2020-06-27 11:49:02 -0300

    Substituir na viewmodel o essencial pela implementação do storage

--- SoundCloudClone/ViewModels/ThemeViewModel.cs
+++ SoundCloudClone/ViewModels/ThemeViewModel.cs
@@ -4,27 +4,33 @@ using MvvmHelpers;
 using MvvmHelpers.Commands;
 using SoundCloudClone.Interfaces;
 using SoundCloudClone.Models.App;
-using Xamarin.Essentials;
 using SoundCloudClone.Enums;
 
 namespace SoundCloudClone.ViewModels
 {
     public class ThemeViewModel : BaseViewModel
     {
+        private readonly IStorage _storage;
+
         public IList<Theme> Options { get; private set; }
         public Theme SelectedOption { get; set; }
         public Command OptionSelectedCommand { get; private set; }
 
         public ThemeViewModel(ITheme theme, IStorage storage)
         {
+            _storage = storage;
+
             Options = theme.GetOptions();
             OptionSelectedCommand = new Command(OptionSelected);
+
+            var value = _storage.Get("selected_theme", (int)ThemeEnum.NonSelected);
         }
 
         private void OptionSelected()
         {
             Options.FirstOrDefault(option => option.IsSelected)?.UnSelect();
             SelectedOption?.Select();
+            _storage.Set("selected_theme", (int)SelectedOption.Name);
         }
     }
 }
