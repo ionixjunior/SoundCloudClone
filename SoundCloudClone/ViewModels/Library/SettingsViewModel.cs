8e3fd7ed62304c1ec26fb704c36eebaa1b6d3538
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-04 11:11:53 -0300

    Criar view model para tela settings

---
+++ SoundCloudClone/ViewModels/Library/SettingsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SoundCloudClone.Enums;
+using SoundCloudClone.Interfaces;
+
+namespace SoundCloudClone.ViewModels.Library
+{
+    public class SettingsViewModel
+    {
+        public ThemeEnum SelectedTheme { get; private set; }
+
+        public SettingsViewModel(IStorage storage)
+        {
+            var themeValue = storage.Get(Constants.SelectedThemeKey, (int)ThemeEnum.NonSelected);
+            SelectedTheme = (ThemeEnum)themeValue;
+        }
+    }
+}
