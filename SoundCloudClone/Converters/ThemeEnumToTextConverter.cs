07e8b4a23c4323964c3623b0c7332cc4723052ea
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-04 11:01:15 -0300

    Implementar conversor para transformar ThemeEnum em texto

---
+++ SoundCloudClone/Converters/ThemeEnumToTextConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using SoundCloudClone.Enums;
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Converters
+{
+    public class ThemeEnumToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ThemeEnum themeEnum)
+            {
+                return themeEnum switch
+                {
+                    ThemeEnum.Light => "Light",
+                    ThemeEnum.Dark => "Dark",
+                    ThemeEnum.System => "System",
+                    ThemeEnum.BatterySaver => "Set by Battery Saver",
+                    _ => string.Empty
+                };
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
