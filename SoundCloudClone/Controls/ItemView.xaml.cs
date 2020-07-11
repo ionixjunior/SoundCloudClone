83696f658e8fe46d88ba7f92d24e8b5698fa3eec
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-11 10:33:19 -0300

    Criar custom control para items de configurações

---
+++ SoundCloudClone/Controls/ItemView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Controls
+{
+    public partial class ItemView : FlexLayout
+    {
+        public ItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
