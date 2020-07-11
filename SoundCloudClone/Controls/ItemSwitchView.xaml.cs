ef2d0d6f7b8df73d0639d1ce34c49aa5c3917295
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-11 12:29:14 -0300

    Implementar controle do switch view

---
+++ SoundCloudClone/Controls/ItemSwitchView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms;
+
+namespace SoundCloudClone.Controls
+{
+    public partial class ItemSwitchView : FlexLayout
+    {
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(ItemSwitchView),
+            string.Empty,
+            propertyChanged: OnTextChanged
+        );
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        private static void OnTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as ItemSwitchView).TextLabel.Text = (string)newValue;
+        }
+
+        public static readonly BindableProperty IsToggledProperty = BindableProperty.Create(
+            nameof(IsToggled),
+            typeof(bool),
+            typeof(ItemSwitchView),
+            false,
+            propertyChanged: OnIsToggledChanged
+        );
+
+        public bool IsToggled
+        {
+            get => (bool)GetValue(IsToggledProperty);
+            set => SetValue(IsToggledProperty, value);
+        }
+
+        private static void OnIsToggledChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as ItemSwitchView).ToggleSwitch.IsToggled = (bool)newValue;
+        }
+
+        public ItemSwitchView()
+        {
+            InitializeComponent();
+        }
+    }
+}
