bcf3c6b22d19da06496c7e0105eb020451dfafeb
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-11 10:52:03 -0300

    Criar bindable property para configurar texto

--- SoundCloudClone/Controls/ItemView.xaml.cs
+++ SoundCloudClone/Controls/ItemView.xaml.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace SoundCloudClone.Controls
 {
     public partial class ItemView : FlexLayout
     {
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(ItemView),
+            string.Empty
+        );
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
         public ItemView()
         {
             InitializeComponent();
