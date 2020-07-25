b573bfcd2b2a9211d71187162607909b5ecdcfe9
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-25 12:29:53 -0300

    Criar método de extensão para converter playlist da api para playlist do app

---
+++ SoundCloudClone/Extensions/PlaylistExtension.cs
@@ -0,0 +1,12 @@
+﻿using SoundCloudClone.Models.App;
+
+namespace SoundCloudClone.Extensions
+{
+    public static class PlaylistExtension
+    {
+        public static Playlist ToPlaylistApp(this SoundCloudClone.Models.Api.Playlist playlistApi)
+        {
+            return new Playlist(playlistApi);
+        }
+    }
+}
