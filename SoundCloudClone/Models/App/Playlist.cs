721ce04109930152aef5e84656aa24558726bea7
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-25 11:44:21 -0300

    Mapear modelos da playlist e das tracks para serem utilizadas no app

---
+++ SoundCloudClone/Models/App/Playlist.cs
@@ -0,0 +1,13 @@
+﻿namespace SoundCloudClone.Models.App
+{
+    public class Playlist
+    {
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public int TrackCount { get; set; }
+        public long Duration { get; set; }
+        public int LikesCount { get; set; }
+        public string Username { get; set; }
+        public string ArtworkUrlTemplate { get; set; }
+    }
+}
---
+++ SoundCloudClone/Models/App/Track.cs
@@ -0,0 +1,11 @@
+﻿namespace SoundCloudClone.Models.App
+{
+    public class Track
+    {
+        public long FullDuration { get; set; }
+        public string Title { get; set; }
+        public int PlaybackCount { get; set; }
+        public string Username { get; set; }
+        public string ArtworkUrlTemplate { get; set; }
+    }
+}
