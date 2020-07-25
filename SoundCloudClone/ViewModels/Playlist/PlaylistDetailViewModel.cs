2da15f76963909d884ca3d4c389666f4558ee69a
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-25 11:35:35 -0300

    Implementar consumo dos detalhes da playlist ao abrir tela

---
+++ SoundCloudClone/ViewModels/Playlist/PlaylistDetailViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using SoundCloudClone.Interfaces;
+
+namespace SoundCloudClone.ViewModels.Playlist
+{
+    public class PlaylistDetailViewModel : IInitialize
+    {
+        private readonly IApi _api;
+
+        public PlaylistDetailViewModel(IApi api)
+        {
+            _api = api;
+        }
+
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                var data = await _api.GetPlaylistDetail();
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }
+        }
+    }
+}
--- SoundCloudClone/Views/Playlist/PlaylistDetailView.xaml.cs
+++ SoundCloudClone/Views/Playlist/PlaylistDetailView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System.Threading.Tasks;
+using SoundCloudClone.Interfaces;
+using SoundCloudClone.ViewModels.Playlist;
+using Xamarin.Forms;
 
 namespace SoundCloudClone.Views.Playlist
 {
@@ -7,6 +10,21 @@ namespace SoundCloudClone.Views.Playlist
         public PlaylistDetailView()
         {
             InitializeComponent();
+
+            var api = DependencyService.Get<IApi>();
+            BindingContext = new PlaylistDetailViewModel(api);
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await OnAppearingAsync();
+        }
+
+        private async Task OnAppearingAsync()
+        {
+            if (BindingContext is IInitialize viewModel)
+                await viewModel.InitializeAsync();
         }
     }
 }
