82e347ff582990765696abac32a542b025de03fd
Ione Souza Junior <junior@ionixjunior.com.br>
2020-07-25 13:18:40 -0300

    Adicionar playlist e tracks na viewmoidel para fazer binding para a view

--- SoundCloudClone/ViewModels/Playlist/PlaylistDetailViewModel.cs
+++ SoundCloudClone/ViewModels/Playlist/PlaylistDetailViewModel.cs
@@ -2,16 +2,23 @@
 using System.Threading.Tasks;
 using SoundCloudClone.Interfaces;
 using SoundCloudClone.Extensions;
+using PlaylistApp = SoundCloudClone.Models.App.Playlist;
+using SoundCloudClone.Models.App;
+using MvvmHelpers;
 
 namespace SoundCloudClone.ViewModels.Playlist
 {
-    public class PlaylistDetailViewModel : IInitialize
+    public class PlaylistDetailViewModel : BaseViewModel, IInitialize
     {
         private readonly IApi _api;
 
+        public PlaylistApp Playlist { get; private set; }
+        public ObservableRangeCollection<Track> Tracks { get; private set; }
+        
         public PlaylistDetailViewModel(IApi api)
         {
             _api = api;
+            Tracks = new ObservableRangeCollection<Track>();
         }
 
         public async Task InitializeAsync()
@@ -19,8 +26,10 @@ namespace SoundCloudClone.ViewModels.Playlist
             try
             {
                 var playlistDetailApi = await _api.GetPlaylistDetail();
-                var playlist = playlistDetailApi.ToPlaylistApp();
-                var tracks = playlistDetailApi.ToTracksApp();
+                Playlist = playlistDetailApi.ToPlaylistApp();
+
+                Tracks.Clear();
+                Tracks.AddRange(playlistDetailApi.ToTracksApp());
             }
             catch (Exception exception)
             {
