5adb3e94d779edca72835252d5871aaeed83ef53
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-22 13:01:43 -0300

    Ajustar posicionamento do container baseado na safe area do iOS

--- SoundCloudClone/Views/OverlayPlaylistLikeTutorialView.xaml.cs
+++ SoundCloudClone/Views/OverlayPlaylistLikeTutorialView.xaml.cs
@@ -1,21 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace SoundCloudClone.Views
 {
     public partial class OverlayPlaylistLikeTutorialView : ContentPage
     {
+        private readonly Rectangle _likeContainerPosition;
+
         public OverlayPlaylistLikeTutorialView(Rectangle likeContainerPosition)
         {
             InitializeComponent();
+            _likeContainerPosition = likeContainerPosition;
+        }
 
-            Container.WidthRequest = likeContainerPosition.Width;
-            Container.HeightRequest = likeContainerPosition.Height;
-            Container.TranslationX = likeContainerPosition.X;
-            //Container.TranslationY = likeContainerPosition.Y + 90;
-            Container.TranslationY = likeContainerPosition.Y + 65;
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Container.WidthRequest = _likeContainerPosition.Width;
+            Container.HeightRequest = _likeContainerPosition.Height;
+            Container.TranslationX = _likeContainerPosition.X;
+            Container.TranslationY = _likeContainerPosition.Y + GetTopHeightSpacing();
+        }
+
+        private double GetTopHeightSpacing()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                const double navbarHeight = 46;
+                return GetSafeAreaInsetTop() + navbarHeight;
+            }
+
+            return 0;
+        }
+
+        private double GetSafeAreaInsetTop()
+        {
+            return On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets().Top;
         }
     }
 }
