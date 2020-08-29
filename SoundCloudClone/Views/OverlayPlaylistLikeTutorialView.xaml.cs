bc46316c7201c643f16a0cf3dd1fa3f47ba20b42
Ione Souza Junior <junior@ionixjunior.com.br>
2020-08-29 12:38:01 -0300

    Criar overlay do tutorial usando code behind

--- SoundCloudClone/Views/OverlayPlaylistLikeTutorialView.xaml.cs
+++ SoundCloudClone/Views/OverlayPlaylistLikeTutorialView.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Shapes;
 
 namespace SoundCloudClone.Views
 {
     public partial class OverlayPlaylistLikeTutorialView : ContentPage
     {
-        private readonly Rectangle _likeContainerPosition;
+        private readonly Xamarin.Forms.Rectangle _likeContainerPosition;
 
-        public OverlayPlaylistLikeTutorialView(Rectangle likeContainerPosition)
+        public OverlayPlaylistLikeTutorialView(Xamarin.Forms.Rectangle likeContainerPosition)
         {
             InitializeComponent();
             _likeContainerPosition = likeContainerPosition;
@@ -16,12 +17,64 @@ namespace SoundCloudClone.Views
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            Build();
 
-            Container.WidthRequest = _likeContainerPosition.Width;
-            Container.HeightRequest = _likeContainerPosition.Height;
-            Container.TranslationX = _likeContainerPosition.X;
-            Container.TranslationY = _likeContainerPosition.Y + GetTopHeightSpacing();
-            Container.IsVisible = true;
+
+            //Container.WidthRequest = _likeContainerPosition.Width;
+            //Container.HeightRequest = _likeContainerPosition.Height;
+            //Container.TranslationX = _likeContainerPosition.X;
+            //Container.TranslationY = _likeContainerPosition.Y + GetTopHeightSpacing();
+            //Container.IsVisible = true;
+
+
+            //RectangleOverlay.SetValue(Xamarin.Forms.Shapes.RectangleGeometry.RectProperty, new Rect(0, 0, Width, Height))
+        }
+
+        private void Build()
+        {
+            var ellipseRadius = 60;
+
+            var grid = new Grid
+            {
+                Children =
+                {
+                    new Path
+                    {
+                        Fill = Brush.Black,
+                        Opacity = 0.8,
+                        Data = new GeometryGroup
+                        {
+                            Children =
+                            {
+                                new RectangleGeometry
+                                {
+                                    Rect = new Rect(0, 0, Width, Height)
+                                },
+                                new EllipseGeometry
+                                {
+                                    RadiusX = ellipseRadius,
+                                    RadiusY = ellipseRadius,
+                                    Center = new Point(_likeContainerPosition.X, _likeContainerPosition.Y + GetTopHeightSpacing())
+                                }
+                            }
+                        }
+                    },
+                    new Ellipse
+                    {
+                        Stroke = new SolidColorBrush(Color.FromHex("F4570B")),
+                        WidthRequest = ellipseRadius * 2,
+                        HeightRequest = ellipseRadius * 2,
+                        StrokeThickness = 5,
+                        TranslationX = _likeContainerPosition.X - ellipseRadius,
+                        TranslationY = _likeContainerPosition.Y + GetTopHeightSpacing() - ellipseRadius,
+                        HorizontalOptions = LayoutOptions.Start,
+                        VerticalOptions = LayoutOptions.Start
+                    }
+                }
+            };
+
+
+            Content = grid;
         }
 
         private double GetTopHeightSpacing()
