89e0b9a6147b88b1f5b6a112f886afecf40d6a12
Ione Souza Junior <junior@ionixjunior.com.br>
2020-09-19 12:43:23 -0300

    Adicionar referência da view como membro da tela do overlay

--- SoundCloudClone/Views/OverlayPlaylistLikeTutorialView.xaml.cs
+++ SoundCloudClone/Views/OverlayPlaylistLikeTutorialView.xaml.cs
@@ -10,11 +10,13 @@ namespace SoundCloudClone.Views
     public partial class OverlayPlaylistLikeTutorialView : ContentPage
     {
         private readonly Xamarin.Forms.Rectangle _likeContainerPosition;
+        private readonly View _likeContainer;
 
-        public OverlayPlaylistLikeTutorialView(Xamarin.Forms.Rectangle likeContainerPosition, View likeContainer)
+        public OverlayPlaylistLikeTutorialView(Xamarin.Forms.Rectangle likeContainerPosition, FlexLayout likeContainer)
         {
             InitializeComponent();
             _likeContainerPosition = likeContainerPosition;
+            _likeContainer = likeContainer;
         }
 
         protected override async void OnAppearing()
