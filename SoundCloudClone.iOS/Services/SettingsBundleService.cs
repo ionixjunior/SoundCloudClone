6f1da608585f76abd9aa023cc713f3bf862004d9
Ione Souza Junior <junior@ionixjunior.com.br>
2020-10-17 11:04:40 -0300

    Adicionar implementação do settings bundle para o iOS

---
+++ SoundCloudClone.iOS/Services/SettingsBundleService.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using SoundCloudClone.Interfaces;
+
+namespace SoundCloudClone.iOS.Services
+{
+    public class SettingsBundleService : ISettingsBundle
+    {
+        public bool GetBoolValue(string key)
+        {
+            return NSUserDefaults.StandardUserDefaults.BoolForKey(key);
+        }
+
+        public void SetBoolValue(bool value, string key)
+        {
+            NSUserDefaults.StandardUserDefaults.SetBool(value, key);
+        }
+    }
+}
