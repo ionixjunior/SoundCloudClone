3ad04f4a639f4df7807dba5759ceefdde5b98179
Ione Souza Junior <junior@ionixjunior.com.br>
2020-10-17 11:02:44 -0300

    Implementar interface para manipulação do settings bundle

---
+++ SoundCloudClone/Interfaces/ISettingsBundle.cs
@@ -0,0 +1,8 @@
+﻿namespace SoundCloudClone.Interfaces
+{
+    public interface ISettingsBundle
+    {
+        bool GetBoolValue(string key);
+        void SetBoolValue(bool value, string key);
+    }
+}
