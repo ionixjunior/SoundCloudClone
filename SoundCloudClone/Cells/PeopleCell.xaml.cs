8ec53e8069c33ebed1976b98de65aedd76a0be02
Ione Souza Junior <junior@ionixjunior.com.br>
2020-11-07 13:15:42 -0300

    Criar célula para exibir a pessoa (people)

---
+++ SoundCloudClone/Cells/PeopleCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Cells
+{
+    public partial class PeopleCell : ContentView
+    {
+        public PeopleCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
