8776ac46fbaa539602aaa4f897e0348f30442ec7
Ione Souza Junior <junior@ionixjunior.com.br>
2020-11-14 11:06:18 -0300

    Adicionar layout do item da playlist

---
+++ SoundCloudClone/Cells/PlaylistCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SoundCloudClone.Cells
+{
+    public partial class PlaylistCell : ContentView
+    {
+        public PlaylistCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
