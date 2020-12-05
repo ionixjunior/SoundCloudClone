a8fbdd08d57753ff716332c32fcb72695b925908
Ione Souza Junior <junior@ionixjunior.com.br>
2020-12-05 13:30:45 -0300

    Mapear classe User no domínio do app

---
+++ SoundCloudClone/Models/App/User.cs
@@ -0,0 +1,22 @@
+﻿namespace SoundCloudClone.Models.App
+{
+    public class User
+    {
+        public string Username { get; }
+        public string AvatarUrl { get; }
+        public string AvatarUrlTemplate { get; }
+        public int FollowersCount { get; }
+        public string City { get; }
+        public string Country { get; }
+
+        public User(SoundCloudClone.Models.Api.User user)
+        {
+            Username = user.Username;
+            AvatarUrl = user.AvatarUrl;
+            AvatarUrlTemplate = user.AvatarUrlTemplate;
+            FollowersCount = user.FollowersCount;
+            City = user.City;
+            Country = user.Country;
+        }
+    }
+}
