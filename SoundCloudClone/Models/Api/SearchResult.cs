4167643e4507c89a9fe80e78678ea47ed91aa7cc
Ione Souza Junior <junior@ionixjunior.com.br>
2020-12-05 12:10:38 -0300

    Remover classes não utilizadas

--- SoundCloudClone/Models/Api/SearchResult.cs
+++ SoundCloudClone/Models/Api/SearchResult.cs
@@ -4,54 +4,6 @@ using Newtonsoft.Json;
 
 namespace SoundCloudClone.Models.Api
 {
-    public class Related
-    {
-        [JsonProperty("href")]
-        public string Href { get; set; }
-    }
-
-    public class Links
-    {
-        [JsonProperty("related")]
-        public Related Related { get; set; }
-    }
-
-    public class Format
-    {
-        [JsonProperty("protocol")]
-        public string Protocol { get; set; }
-
-        [JsonProperty("mime_type")]
-        public string MimeType { get; set; }
-    }
-
-    public class Transcoding
-    {
-        [JsonProperty("url")]
-        public string Url { get; set; }
-
-        [JsonProperty("preset")]
-        public string Preset { get; set; }
-
-        [JsonProperty("duration")]
-        public int Duration { get; set; }
-
-        [JsonProperty("snipped")]
-        public bool Snipped { get; set; }
-
-        [JsonProperty("format")]
-        public Format Format { get; set; }
-
-        [JsonProperty("quality")]
-        public string Quality { get; set; }
-    }
-
-    public class Media
-    {
-        [JsonProperty("transcodings")]
-        public IList<Transcoding> Transcodings { get; set; }
-    }
-
     public class CollectionSearchResult
     {
         [JsonProperty("playlist")]
