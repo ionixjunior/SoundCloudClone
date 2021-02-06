87761b512e409f8c9bd9b522fcc5c84230c85ee8
Ione Souza Junior <junior@ionixjunior.com.br>
2021-02-06 12:17:11 -0300

    Popular valores na AlbumCell baseado no contexto de binding do SearchResult

--- SoundCloudClone/Cells/AlbumCell.xaml.cs
+++ SoundCloudClone/Cells/AlbumCell.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using SoundCloudClone.Models.App;
 using Xamarin.Forms;
 
 namespace SoundCloudClone.Cells
@@ -9,6 +10,21 @@ namespace SoundCloudClone.Cells
         public AlbumCell()
         {
             InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            if (BindingContext is SearchResult searchResult && searchResult.Data is Playlist playlist && playlist.IsAlbum)
+            {
+                ArtworkUrlImage.Source = playlist.ArtworkUrl;
+                UsernameLabel.Text = playlist.Username;
+                TrackCountLabel.Text = $"{playlist.TrackCount} tracks";
+                TitleLabel.Text = playlist.Title;
+                DescriptionLabel.Text = playlist.Description;
+                LikesCountLabel.Text = playlist.LikesCount.ToString();
+            }
         }
     }
 }
